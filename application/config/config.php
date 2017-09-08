c3f9c27faf80d462197f2e7c3a53fe04458c74ad
Avillanueva <a_ngel_456@hotmail.com>
2017-09-08 12:38:48 -0500

    ui

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Convocatoria/';
+$config['base_url'] = 'https://mysterious-dawn-98389.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
